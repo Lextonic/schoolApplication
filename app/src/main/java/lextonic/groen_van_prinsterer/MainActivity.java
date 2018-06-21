b094ba9c883ed727222f8363ddf13aa1d441ebf1
Lextonic <31688485+Lextonic@users.noreply.github.com>
2018-06-22 01:06:50 +0200

    Lextonic
    Created a welcome screen.

--- app/src/main/java/lextonic/groen_van_prinsterer/MainActivity.java
+++ app/src/main/java/lextonic/groen_van_prinsterer/MainActivity.java
@@ -1,5 +1,7 @@
 package lextonic.groen_van_prinsterer;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,26 +10,24 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout mDrawerlayout;
-    private ActionBarDrawerToggle mToggle;
+
+    private static int SPLASH_TIME_OUT = 4000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
-        mDrawerlayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        setContentView(R.layout.activity_home);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected((item))) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
     }
 }
